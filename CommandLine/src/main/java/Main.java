3a408ec3ec0aeebd3fe59714f4079fea21e3d2bd
Aleksa <adjokic1619rn@raf.rs>
2021-11-01 15:15:46 +0100

    Add basic functionalities to main

--- CommandLine/src/main/java/Main.java
+++ CommandLine/src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello CommandLine");
+    public static void main(String[] args) throws IOException {
+        String path = "/home/aleksa";
+        System.out.println("Trenutni dir: " + path);
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String niz[] = s.split(" ");
+        //Main objekat = new Main();
+        if (niz[0].equals("cd")) {
+            path += niz[1];
+        }
+        else if (niz[0].equals("touch")) {
+            LocalImplementation local = new LocalImplementation();
+            local.createFile(niz[1],path);
+        }
+        else if (niz[0].equals("rm")) {
+            LocalImplementation local = new LocalImplementation();
+            local.deleteFile(niz[1],path);
+        }
+
+        //noviFajl.mkdir();
     }
+
 }
