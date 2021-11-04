c9e14b52ea481b5b3377b88a86742b8cc181369d
estojanovic <estojanovic8720rn@raf.rs>
2021-11-04 11:51:35 +0100

    LocalImpl methods

--- CommandLine/src/main/java/Main.java
+++ CommandLine/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     static LocalImplementation local = new LocalImplementation();
@@ -10,13 +12,16 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         String path = "";
         String os = "";
+
         Scanner in = new Scanner(System.in);
         System.out.println("Unesite putanju do skladista: ");
         String s = in.nextLine();
-        if (s.contains("/")) os="/";
-        else os = "\"";
+
+
         Path putanja = Paths.get(s);
-        System.out.println(Files.exists(putanja));
+        System.out.println("separator: "+ File.separator);
+        os=File.separator;
+
 
         if (Files.exists(putanja) == false) {
             System.out.println("Ako zelite da napravite novo skladiste ukucajte ns i putanju do skladista");
@@ -34,6 +39,8 @@ public class Main {
                 System.out.println("Unesite velicinu skladista");
                 Long storageSize = Long.parseLong(in.nextLine());
                 local.createStorage(ime, path, storageSize);
+                //path+=os;
+                path+=ime;
             }
 
             if (niz[0].equals("cd")) {
@@ -42,24 +49,65 @@ public class Main {
                 System.out.println(path);
             }
             else if (niz[0].equals("touch")) {
-                path += os;
+                String path1=path+os;
+                //path += os;
                 //Class local = Class.forName("LocalImplementation");// = new LocalImplementation();
-                local.createFile(niz[1],path);
+                local.createFile(niz[1],path1);
+                System.out.println(path);
+            }
+            else if (niz[0].equals("mkdir")) {
+                // String path1= path+ os;
+                path+=os;
+                String name = niz[1];
+                //Class local = Class.forName("LocalImplementation");// = new LocalImplementation();
+                local.createDirectory(name,path);
+
+                path+=name;
+                System.out.println(path);
             }
             else if (niz[0].equals("rm")) {
-                local.deleteFile(niz[1],path);
+                String path1=path+ os;
+                local.deleteFile(niz[1],path1);
             }
 
             if (niz[0].equals("..")) {
-                String niz1[] = s.split(os);
+                String niz1[] = path.split(Pattern.quote(os));
                 path = "";
-                for (int i=0 ; i < niz1.length - 1; i++)
+                for (int i=0 ; i < niz1.length-1 ; i++)
                 {
                     path += niz1[i];
                     if (i != niz1.length - 2) path += os;
                 }
                 System.out.println(path);
             }
+            if (niz[0].equals("move")) {
+                String currentpath=path+os;
+
+                //move filename
+                String filename=niz[1];
+                System.out.println("Unesite putanju nove lokacije:");
+                path =in.nextLine();
+                String path1=path+os;
+
+                local.moveFile(filename,path1,currentpath);
+                System.out.println("u metodi move sam "+ path);
+            }
+            if (niz[0].equals("ls")) {
+                local.listFilesFromDirectory(path);
+            }
+            if (niz[0].equals("save")) {
+                String filename=niz[1];
+
+                System.out.println("u save metodi sam "+path);
+                local.saveFile(filename,path);
+            }
+            if (niz[0].equals("?")) {
+                System.out.println(path);
+            }
+            if (niz[0].equals("exit")) {
+                System.exit(0);
+            }
+
         }
 
         //noviFajl.mkdir();
--- LocalImplementation/src/main/java/LocalImplementation.java
+++ LocalImplementation/src/main/java/LocalImplementation.java
@@ -2,10 +2,18 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 public class LocalImplementation extends SpecificationClass implements SpecificationInterface {
     HashMap<String, Long> mapOfStorageSizes = new HashMap<>();
+    String os=File.separator;
+
 
     static {
         SpecificationManager.registerExporter(new LocalImplementation());
@@ -52,13 +60,26 @@ public class LocalImplementation extends SpecificationClass implements Specifica
     }
 
     @Override
-    public void moveFile(String s, String s1, String s2) {
+    public void moveFile(String filename, String path, String currentpath) {
+        try {
+            Files.move(Paths.get(currentpath+filename) ,Paths.get(path+filename), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
-    public void saveFile(String s, String s1) {
+    public void saveFile(String filename, String path) {
+        //moveFile(filename, System.getProperty("user.home")+os, path+os);
+        try {
+            Path newDir=Paths.get(System.getProperty("user.home"));
+            Files.copy(Paths.get(path+os+filename), newDir.resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Kopirao sam: "+filename);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -75,7 +96,34 @@ public class LocalImplementation extends SpecificationClass implements Specifica
 
 
     @Override
-    public void listFilesFromDirectory(String s) {
+    public void listFilesFromDirectory(String path) {
+        File file = new File(path);
+        File[] list=file.listFiles();
+        if (list != null) {
+            for (File f : list) {
+
+                System.out.println(f.getName());
+                // System.out.println(f.length());
+
+                BasicFileAttributes attrs = null;
+                try {
+                    attrs = Files.readAttributes(f.toPath(), BasicFileAttributes.class);
+                    FileTime time = attrs.creationTime();
+                    String pattern = "yyyy-MM-dd HH:mm:ss";
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+                    String formatted = simpleDateFormat.format( new Date( time.toMillis() ) );
+
+                    // System.out.println(  f.getName()+" je kreiran " + formatted );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                //System.out.println(f); printuje celu putanju
+
+            }
+        }
 
     }
 
